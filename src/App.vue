ba2e42b58b414afd8163a60e736487a3871ddc37
Dev NhaX <nhanguyen7901@gmail.com>
2023-05-01 10:04:11 +0700

    migration-task-list-to-its-own-file/folder

--- src/App.vue
+++ src/App.vue
@@ -19,31 +19,7 @@
 </template>
 
 <script>
-import { taskLevel } from "./enums/TaskLevel";
-
-const taskList = [
-  {
-    id: 1,
-    name: "Learn Vue",
-    desc: "Learn component, router",
-    level: taskLevel.Easy,
-    isCompleted: true,
-  },
-  {
-    id: 2,
-    name: "Learn Vue",
-    desc: "Learn Vuex",
-    level: taskLevel.Medium,
-    isCompleted: false,
-  },
-  {
-    id: 3,
-    name: "Learn Nuxt",
-    desc: "Create a first API",
-    level: taskLevel.Hard,
-    isCompleted: false,
-  },
-];
+import taskList from './data/taskList';
 
 export default {
   name: "App",
