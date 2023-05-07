f0246659bf3108ef3403672a89666b76ce7684f8
Dev NhaX <nhanguyen7901@gmail.com>
2023-05-07 22:00:01 +0700

    add-user-to-navbar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
 <template>
   <div>
     <header>
-      <h1 class="text-vue text-7xl font-bold text-center p-10">Todo App</h1>
+      <div class="flex justify-end mr-10">
+        <router-link :to="{ name: 'login' }">
+          <button class="pt-3">
+            <span v-html="getUserSvg"></span>
+          </button>
+        </router-link>
+      </div>
+      <div class="p-10 pt-3">
+        <div class="text-center">
+          <router-link :to="{ name: 'home' }">
+            <h1 class="text-vue text-7xl font-bold inline-block">Todo App</h1>
+          </router-link>
+        </div>
+      </div>
     </header>
     <main>
-      <router-view></router-view>
+      <transition name="slide" mode="out-in">
+        <router-view></router-view>
+      </transition>
     </main>
   </div>
 </template>
 
 <script>
+import { iconList } from "./enums/Icon";
+
 export default {
   name: "App",
+  computed: {
+    getUserSvg() {
+      return iconList.user;
+    },
+  },
 };
 </script>
 
@@ -27,5 +49,16 @@ export default {
 }
 .bg-orange {
   background-color: orange;
+}
+
+.slide-enter-active,
+.slide-leave-active {
+  transition: opacity 0.5s, transform 0.5s;
+}
+
+.slide-enter,
+.slide-leave-to {
+  opacity: 0;
+  transform: translateX(-30%);
 }
 </style>
