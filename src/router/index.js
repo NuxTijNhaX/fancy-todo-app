02a188a8cf9a1959fa8e63eba858984173aa0938
Dev NhaX <nhanguyen7901@gmail.com>
2023-05-08 08:16:39 +0700

    add-require-authen-and-beforeEach

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,11 @@ const routes = [
     component: HomeView,
   },
   {
+    path: "/login",
+    name: "login",
+    component: () => import("@/views/LoginView.vue"),
+  },
+  {
     path: "/add-task",
     name: "add-task",
     // route level code-splitting
@@ -19,13 +24,17 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AddTaskView.vue"),
+    meta: { requireAuthen: true },
   },
   {
     path: "/edit/:id",
     name: "edit-task",
     component: () => import("../views/EditTaskView.vue"),
+    meta: { requireAuthen: true },
     beforeEnter: (to, from, next) => {
-      const isExisted = store.state.taskList.find((task) => task.id === to.params.id);
+      const isExisted = store.state.taskList.find(
+        (task) => task.id === to.params.id
+      );
 
       if (isExisted) next();
       else next({ name: "not-found" });
@@ -35,6 +44,7 @@ const routes = [
     path: "/delete/:id",
     name: "delete-task",
     component: () => import("../views/DeleteTaskView.vue"),
+    meta: { requireAuthen: true },
   },
   {
     path: "/not-found",
@@ -48,6 +58,19 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  // if (to.matched.some((record) => record.meta.requireAuthen)) {
+  if (to.meta.requireAuthen) {
+    if (!store.state.user) {
+      next({ name: "login", query: { redirect: to.fullPath } });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
